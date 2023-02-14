f3e901d3ec60561b18e09eae59a03404c0822ec2
Shane Jaroch <chown_tee@proton.me>
2023-02-14 00:25:03 -0500

    wip
        DEBUG print csv path

--- chessdet/sheetutils.py
+++ chessdet/sheetutils.py
@@ -8,7 +8,7 @@ import csv
 
 import requests
 
-from chessdet import CSV_GAMES_FILE_PATH
+from chessdet import CLI_CONFIG, CSV_GAMES_FILE_PATH
 from chessdet.env import CSV_GAMES_URL
 
 
@@ -40,6 +40,8 @@ def cache_csv_games_file(
 
 def build_csv_reader(csv_file_path: str = CSV_GAMES_FILE_PATH) -> csv.DictReader:
     """Returns a csv.reader() object"""
+    if CLI_CONFIG.debug:
+        print(f"Using csv_file_path={csv_file_path}")
 
     # pylint: disable=consider-using-with
     reader = csv.DictReader(open(csv_file_path, encoding="utf-8"))
